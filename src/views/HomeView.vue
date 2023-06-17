badadc49af5e87baba9d3fb84ff9e7c6c53ffc2c
qnfthslr <105690509+qnfthslr@users.noreply.github.com>
2023-06-17 11:46:50 +0900

    update: 홈화면 링크 추가

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,13 +2,12 @@
   <v-container>
     <nav>
       <router-link to="/account-register-page">계정 등록 </router-link> |
-      <router-link to="/account-list-page">계정 목록 </router-link> |
+      <router-link to="/account-list-page">계정 등록 </router-link> |
     </nav>
   </v-container>
 </template>
 
 <script>
-
 
 export default {
   name: 'Home',
