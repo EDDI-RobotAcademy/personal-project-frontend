cc4e5d7e31199b3591e7e9c5788d8f9e41f05459
qnfthslr <105690509+qnfthslr@users.noreply.github.com>
2023-06-20 17:42:41 +0900

    update: HomeView 링크 수정

--- src/router/account/index.js
+++ src/router/account/index.js
@@ -1,23 +1,22 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 
-import AccountRegisterPage from "@/views/account/AccountRegisterPage.vue"
-import AccountListPage from "@/views/account/AccountListPage.vue"
+import AccountRegisterPage from "@/views/account/AccountRegisterPage.vue";
+import AccountListPage from "@/views/account/AccountListPage.vue";
 
 Vue.use(VueRouter);
 
 const accountRoutes = [
+  {
+    path: "/account-register-page",
+    name: "AccountRegisterPage",
+    component: AccountRegisterPage,
+  },
 
-    {
-        path: "/account-register-page",
-        name: "AccountRegisterPage",
-        component: AccountRegisterPage
-    },
-    {
-        path: "/account-list-page",
-        name: "AccountListPage",
-        component: AccountListPage
-    },
-
+  {
+    path: "/manager-list-page",
+    name: "AccountListPage",
+    component: AccountListPage,
+  },
 ];
 export default accountRoutes;
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,23 @@
 <template>
   <v-container>
     <nav>
-      <router-link to="/account-register-page">노말 계정 등록</router-link> |
-      <router-link to="/account-list-page">관리자용 계정 목록</router-link> |
+      <router-link class="complete" to="/account-register-page">회원 계정 등록</router-link> |
+      <router-link to="/account-find-password-page">비밀번호 찾기 page</router-link> |
+      <router-link to="/account-notice-page">회원 공지사항 page</router-link> |
+      <router-link to="/account-event-page">회원 이벤트 page</router-link> |
+      <router-link to="/account-shop-page">샾 page</router-link> |
+      <router-link to="/community-page">community page</router-link> |
+
+      <br><br>
+      <hr>
+      <br>
+
+      <router-link to="/way-to-come-page">오시는 길 page</router-link> |
+      <router-link class="complete" to="/manager-list-page">관리자용 계정 목록</router-link> |
+      <router-link to="/manager-notice-page">관리자용 공지사항 page</router-link> |
+      <router-link to="/manager-event-page">관리자용 이벤트 page</router-link> |
+      <router-link to="/manager-shop-page">관리자용 샾 page</router-link> |
+
     </nav>
   </v-container>
 </template>
@@ -15,3 +30,10 @@ export default {
   },
 }
 </script>
+
+<style scoped>
+.complete {
+  color: red;
+}
+</style>
+
