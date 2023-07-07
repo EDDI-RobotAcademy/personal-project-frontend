fbb704fce068e7d26ca3a097fabc68517209653d
YeoulCho <cyu5733@gmail.com>
2023-07-07 17:24:09 +0900

    add: [조여울] 게시판 리스트페이지 커밋 누락[CGV-4]

--- src/views/board/MemberBoardListPage.vue
+++ src/views/board/MemberBoardListPage.vue
@@ -1,16 +1,33 @@
 <template lang="">
     <div>
-        <member-board-list-form/>
+        <member-board-list-form :boards="boards"/>
     </div>
 </template>
+
 <script>
 import MemberBoardListForm from '../../components/board/MemberBoardListForm.vue';
+
+import { mapActions, mapState } from 'vuex';
+
+const boardModule = 'boardModule'
+
 export default {
     components:{
         MemberBoardListForm
+    },
+    computed: { 
+        ...mapState(boardModule, ['boards']),
+    },
+    mounted () {
+        this.requestBoardListToSpring()
+    },
+    methods: {
+        ...mapActions(
+            boardModule, ['requestBoardListToSpring']
+        )
     }
 }
 </script>
-<style lang="">
+<style>
     
 </style>
