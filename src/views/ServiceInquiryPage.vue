9f66aea38393e04b0c84770e7bf1a2155f6cd738
jinho1593 <50562662+jinho1593@users.noreply.github.com>
2023-07-09 17:42:06 +0900

    Feat :[김진호] 서비스 페이지 만들기[VRS-6]

---
+++ src/views/ServiceInquiryPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="service-container">
+    <ServiceHeader />
+  </div>
+</template>
+
+<script>
+import ServiceHeader from '@/components/service/ServiceHeader.vue';
+export default {
+  components: {
+    ServiceHeader,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.service-container {
+  margin-top: 180px;
+}
+</style>
