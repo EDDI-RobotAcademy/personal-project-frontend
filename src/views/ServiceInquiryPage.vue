26c935d933e766ff41c85123ca68e5fed2358eff
jinho1593 <50562662+jinho1593@users.noreply.github.com>
2023-07-09 18:06:46 +0900

    Feat :[김진호] 1대1 문의 등록 페이지 구현[VRS-7]

--- src/views/ServiceInquiryPage.vue
+++ src/views/ServiceInquiryPage.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="service-container">
     <ServiceHeader />
+    <InquiryForm />
   </div>
 </template>
 
 <script>
 import ServiceHeader from '@/components/service/ServiceHeader.vue';
+import InquiryForm from '@/components/service/InquiryForm.vue';
 export default {
   components: {
     ServiceHeader,
+    InquiryForm,
   },
 };
 </script>
