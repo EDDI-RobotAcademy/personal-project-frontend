1e05ce27d243b6955f45c60ecd2a9511ca225d9d
NamHyunKi <128335068+NamHyunKi@users.noreply.github.com>
2023-07-12 21:16:16 +0900

    add: [남현기] 회원가입 페이지 디자인 [OPF-1]

--- src/components/account/AccountRegisterForm.vue
+++ src/components/account/AccountRegisterForm.vue
@@ -2,37 +2,36 @@
     <div style="font-family: 'Caudex-Regular'; margin-top: 220px;">
         <v-row justify="center">
             <v-col cols="auto">
-                <v-card width="460" style="background-color: rgba(255, 255, 255, 0.8);">
-                    <v-card-text class="text-center px-12 py-16" style="margin-top: 75px;">
+                <v-card width="400"
+                    style="background-color: rgba(255, 255, 255, 0.8); border: 3px solid #000000; border-radius: 25px;">
+                    <v-card-text class="text-center px-12 py-16" style="margin-top: 10px;">
                         <v-form @submit.prevent="registAccount" ref="form">
-                            <div style="margin-bottom: 55px; margin-top: -25px; font-size: 38px;">회원 가입
-                                <v-divider style="margin-top: 16px;"></v-divider>
+                            <div style="margin-bottom: 30px; margin-top: -25px; font-size: 38px; color: #000000;">회원 가입
+                                <v-divider style="margin-top: 16px; border: 2px solid #000000;"></v-divider>
                             </div>
                             <div class="d-flex">
-                                <v-text-field v-model="email" label="이메일" :rules="email_rule" :disabled="false" required>
-                                </v-text-field>
-                                <v-btn text large outlined style="font-size: 13px" class="mt-3 ml-5" color="brown"
+                                <v-text-field v-model="email" label="이메일" :rules="email_rule" :disabled="false"
+                                    required></v-text-field>
+                                <v-btn text large outlined style="font-size: 13px" class=" mt-3 ml-5" color="black"
                                     @click="checkDuplicateEmail" :disabled="false">
-                                    이메일 <br /> 중복 확인
+                                    이메일 <br> 중복 확인
                                 </v-btn>
                             </div>
                             <div class="d-flex">
                                 <v-text-field v-model="nickname" label="닉네임" :minlength="2" :maxlength="10"
-                                    :disabled="false" required>
-                                </v-text-field>
-                                <v-btn text large outlined style="font-size: 13px" class="mt-3 ml-5" color="brown"
+                                    :disabled="false" required></v-text-field>
+                                <v-btn text large outlined style="font-size: 13px" class="mt-3 ml-5" color="black"
                                     @click="checkDuplicateNickName" :disabled="false">
-                                    닉네임 <br /> 중복 확인
+                                    닉네임 <br> 중복 확인
                                 </v-btn>
                             </div>
                             <v-text-field v-model="password" label="비밀번호" :disabled="false" type="password"
-                                :rules="password_rule">
-                            </v-text-field>
+                                :rules="password_rule"></v-text-field>
                             <v-text-field v-model="realPassword" label="비밀번호 확인" :disabled="false" type="password"
-                                :rules="password_confirm_rule">
-                            </v-text-field>
+                                :rules="password_confirm_rule"></v-text-field>
                             <v-btn type="submit" block x-large rounded color="gray lighten-1" class="mt-6"
-                                :disabled="!isEmailValid() || !isPasswordValid() || !isNicknameValid()">
+                                :disabled="!isEmailValid() || !isPasswordValid() || !isNicknameValid()"
+                                style="border-radius: 5px;">
                                 회원가입
                             </v-btn>
                         </v-form>
@@ -42,6 +41,7 @@
         </v-row>
     </div>
 </template>
+  
 
 <script>
 import { mapActions } from 'vuex';
