32c157bc764d29052d921fefd3aad19657920b77
NamHyunKi <128335068+NamHyunKi@users.noreply.github.com>
2023-07-13 15:21:45 +0900

    feat: [남현기] 홈 화면 플레이리스트 목록 표시 [OPF-8]

---
+++ src/components/HomeViewForm.vue
@@ -0,0 +1,41 @@
+<template lang="">
+  <v-container>
+    <div>
+      <h3>플레이리스트 목록</h3>
+      <table>
+        <tr>
+          <th align="center" width="70%">제목</th>
+        </tr>
+        <tr v-if="!playlists || (Array.isArray(playlists) && playlists.length === 0)">
+          <td colspan="4">
+            현재 등록된 플레이리스트가 없습니다!
+          </td>
+        </tr>
+        <tr v-else v-for="playlist in playlists" :key="playlist.id">
+          <td align="center">
+            <router-link :to="{
+              name: 'PlaylistReadPage',
+              params: { id: playlist.id.toString() }
+            }">
+              {{ playlist.title }}
+            </router-link>
+          </td>
+        </tr>
+      </table>
+    </div>
+  </v-container>
+</template>
+
+<script>
+
+export default {
+  props: {
+    playlists: {
+      type: Array
+    }
+  },
+  data: () => ({
+  }),
+
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,27 @@
 <template>
-  <hello-world />
+  <home-view-form :playlists="playlists" />
 </template>
 
 <script>
-import HelloWorld from '../components/HelloWorld'
+import HomeViewForm from '../components/HomeViewForm'
+import { mapActions, mapState } from 'vuex'
+
+const playlistModule = 'playlistModule'
 
 export default {
   name: 'Home',
 
   components: {
-    HelloWorld,
+    HomeViewForm,
   },
+  methods: {
+    ...mapActions(playlistModule, ['requestPlaylistListToSpring']),
+  },
+  async mounted() {
+    await this.requestPlaylistListToSpring()
+  },
+  computed: {
+    ...mapState(playlistModule, ['playlists'])
+  }
 }
 </script>
