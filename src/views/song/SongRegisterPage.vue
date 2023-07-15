bc7ed7f95063e5c28c8ab75ce5efabeaa7198c33
NamHyunKi <128335068+NamHyunKi@users.noreply.github.com>
2023-07-15 20:08:00 +0900

    fix: [남현기] 노래 등록 시 playlistId 값 전달 이슈 해결 [OPF-12]

--- src/views/song/SongRegisterPage.vue
+++ src/views/song/SongRegisterPage.vue
@@ -1,7 +1,9 @@
 <template lang="">
     <div>
         <song-register-form @submit="registerSong"/>
+        {{playlistId}}
     </div>
+    
 </template>
 <script>
 import SongRegisterForm from '@/components/song/SongRegisterForm.vue'
@@ -10,6 +12,12 @@ import { mapActions } from 'vuex';
 const songModule = 'songModule'
 
 export default {
+    props: {
+        playlistId: {
+            type: String,
+            required: true,
+        }
+    },
     components: {
         SongRegisterForm,
     },
@@ -17,18 +25,17 @@ export default {
         ...mapActions(songModule, ['requestRegisterSongToSpring']),
 
         async registerSong(payload) {
-            const playlistId = this.$route.params.playlistId;
-            // payload.playlistId = playlistId;
-            // console.log(payload)
+            const playlistId = this.playlistId
+            console.log(payload)
+            console.log(playlistId)
             const song = await this.requestRegisterSongToSpring({ payload, playlistId });
             console.log('song: ' + JSON.stringify(song))
             await this.$router.push({
                 name: 'SongReadPage',
-                params: { id: song.id.toString() }
+                params: { id: song.toString() }
             })
         },
     },
-    props: ['playlistId'],
 }
 </script>
 <style lang="">
