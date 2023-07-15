047eb23d73ac359f04598101f8c9930532631b27
NamHyunKi <128335068+NamHyunKi@users.noreply.github.com>
2023-07-15 22:18:26 +0900

    chore: [남현기] actions 주석 추가

--- src/store/song/actions.js
+++ src/store/song/actions.js
@@ -6,12 +6,14 @@ import {
 import axiosInst from '@/utility/axiosInst'
 
 export default {
+    // 노래 리스트 요청
     requestSongListToSpring({ commit }, playlistId) {
         return axiosInst.springAxiosInst.get(`/song/list/${playlistId}`)
             .then((res) => {
                 commit(REQUEST_SONG_LIST_TO_SPRING, res.data)
             })
     },
+    // 등록한 노래 수 요청
     requestSongCountToSpring({ }) {
         return axiosInst.springAxiosInst.post('/song/count-song')
             .then((res) => {
@@ -19,6 +21,7 @@ export default {
                 return res.data
             })
     },
+    // 노래 등록 요청
     requestRegisterSongToSpring(_, { payload, playlistId }) {
         const { title, singer, genre, link } = payload;
         const requestData = {
@@ -34,12 +37,14 @@ export default {
                 alert("문제 발생!")
             });
     },
+    // 특정 노래 정보 요청
     requestSongToSpring({ commit }, id) {
         return axiosInst.springAxiosInst.get(`/song/${id}`)
             .then((res) => {
                 commit(REQUEST_SONG_TO_SPRING, res.data)
             })
     },
+    // 노래 정보 수정 요청
     requestModifySongToSpring({ }, { payload, songId }) {
         const { title, singer, genre, link } = payload
         const requestData = { songId, title, singer, genre, link }
