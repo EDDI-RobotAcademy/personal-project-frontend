fcc702d53c2e50c51717ead553fa196a73f1228d
dusdn943 <128335427+dusdn943@users.noreply.github.com>
2023-07-18 19:24:34 +0900

    feat: [최연우] 사용자의 중복 여부 확인 후 로그인 초기설정 [VCP-2]

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <MyLogin v-if="!loggedIn" @login="handleLogin" />
+    <!-- <div v-else>
+      <h2>Welcome, {{ username }}!</h2>
+      <button @click="logout">Logout</button>
+    </div> -->
+  </div> 
+</template>
+
+<script>
+import MyLogin from './components/MyLogin.vue';
+
+export default {
+  name: 'App',
+  components: {
+    MyLogin
+  },
+  data() {
+    return {
+      loggedIn: false,
+      username: ''
+    };
+  },
+  methods: {
+    handleLogin(username) {
+      this.loggedIn = true;
+      this.username = username;
+    },
+    logout() {
+      this.loggedIn = false;
+      this.username = '';
+    }
+  }
+};
+</script>
---
+++ src/components/MyLogin.vue
@@ -0,0 +1,79 @@
+<template>
+    <div>
+        <div class="navigation-bar">
+            <div class="logo">CATCHCHAIR</div>
+            <div class="login-button">LOGIN</div>
+            <div class="search-button">O</div>
+        </div>
+        <div class="login">로그인</div>
+        <div class="signup">회원가입</div>
+        <input type="text" />
+        <input type="password" />
+        
+        
+        <!-- 로그인 참고 코드 -->
+        <!-- <h2>Login</h2>
+            <form @submit.prevent="login">
+                <label>
+                    Username:
+                    <input type="text" v-model="username" required>
+                </label>
+                <br>
+                <label>
+                    Password:
+                    <input type="password" v-model="password" required>
+                </label>
+                <br>
+                <button type="submit">Login</button>
+            </form> -->
+        </div>
+    </template>
+    
+    <script>
+    // export default {
+    //     data() {
+    //         return {
+    //             username: '',
+    //             password: ''
+    //         };
+    //     },
+    //     methods: {
+    //         login() {
+    //             if (this.username === 'dusdn' && this.password === '1234') {
+    //                 this.$emit('login', this.username);
+    //                 this.username = '';
+    //                 this.password = '';
+    //             } else {
+    //                 alert('Invalid username or password');
+    //             }
+    //         }
+    //     }
+    // };
+</script>
+
+<style>
+.navigation-bar{
+    display: flex;
+    flex-direction: row;
+    margin-top: 20px;
+    width: 90%;
+    justify-content: space-between;
+}
+
+.logo {
+    font-size: 30px;
+    color: #fe8d6f;
+    font-weight: bold;
+    margin-left: 5%;
+}
+
+.login-button{
+    padding: 0px 30px;
+}
+
+.search-button{
+    padding: 0px 30px;
+
+}
+
+</style>
