6cb79ce35b751b85b1477a8d5af59f7b76498a40
NamHyunKi <128335068+NamHyunKi@users.noreply.github.com>
2023-07-22 18:40:26 +0900

    feat: [남현기] css 변경

--- src/views/layout/navigation/NavigationMenuPage.vue
+++ src/views/layout/navigation/NavigationMenuPage.vue
@@ -1,34 +1,45 @@
 <template>
-  <v-app-bar app color="black" flat>
-    <v-container class="py-0 fill-height">
-      <v-avatar class="mr-10" color="white" size="50"></v-avatar>
-
-      <v-btn class="white--text" text @click="goHome">
-        Home
-      </v-btn>
-
-      <v-spacer></v-spacer>
-
-      <v-responsive max-width="400">
-        <router-link to="/account-login-page">
-          <v-btn v-if="!isLogin" color="white" text>로그인
-          </v-btn>
-        </router-link>
-        <router-link to="/account-register-page">
-          <v-btn v-if="!isLogin" color="white" text>회원가입
-          </v-btn>
-        </router-link>
-        <v-btn v-if="isLogin" color="white" text @click="accountLogout">로그아웃
-        </v-btn>
-        <router-link to="/playlist-register-page">
-          <v-btn v-if="isLogin" color="white" text>플레이리스트 등록
-          </v-btn>
-        </router-link>
-        <router-link to="/account-my-page">
-          <v-btn v-if="isLogin" color="white" text>마이페이지
-          </v-btn>
-        </router-link>
-      </v-responsive>
+  <v-app-bar app color="white" flat class="custom-app-bar">
+    <v-container class="fill-height" fluid>
+      <v-row align="center" no-gutters>
+        <v-col>
+          <v-avatar class="mr-5" color="black" size="50"></v-avatar>
+          <router-link to="/">
+            <v-btn class="black--text custom-btn" :style="{ fontSize: '20px' }" text>
+              Home
+            </v-btn>
+          </router-link>
+        </v-col>
+        <v-col>
+          <v-spacer></v-spacer>
+        </v-col>
+        <v-col></v-col>
+        <v-col>
+          <v-row align="center" no-gutters>
+            <v-col cols="auto">
+              <router-link to="/account-login-page">
+                <v-btn v-if="!isLogin" :style="{ fontSize: '17px' }" color="black" class="white--text">로그인
+                </v-btn>
+              </router-link>
+              <router-link to="/account-register-page">
+                <v-btn v-if="!isLogin" :style="{ fontSize: '20px' }" color="black" class="white--text ml-2">회원가입
+                </v-btn>
+              </router-link>
+              <router-link to="/playlist-register-page">
+                <v-btn v-if="isLogin" :style="{ fontSize: '17px' }" color="black" class="white--text mr-2">플레이리스트 등록
+                </v-btn>
+              </router-link>
+              <v-btn v-if="isLogin" :style="{ fontSize: '17px' }" color="black" class="white--text mr-2"
+                @click="accountLogout">로그아웃
+              </v-btn>
+              <router-link to="/account-my-page">
+                <v-btn v-if="isLogin" :style="{ fontSize: '20px' }" color="black" class="white--text">마이페이지
+                </v-btn>
+              </router-link>
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
     </v-container>
   </v-app-bar>
 </template>
@@ -42,12 +53,6 @@ export default {
 
   data: () => ({
     isLogin: localStorage.getItem("isLogin"),
-    links: [
-      'Home',
-      '나',
-      '다',
-      '라',
-    ],
   }),
   methods: {
     ...mapActions(accountModule, ["requestLogoutToSpring"]),
@@ -60,12 +65,35 @@ export default {
           location.reload()
         )
     },
-    goHome() {
-      this.$router.push({ name: 'home' })
-        .catch(
-          location.reload()
-        )
-    },
   },
 };
 </script>
+
+<style>
+.custom-app-bar {
+  height: 130px !important;
+  padding-top: 25px !important;
+  padding-bottom: 5px !important;
+}
+
+.fill-height {
+  height: 100%;
+}
+
+.v-application {
+  height: 100%;
+}
+
+.v-window-item,
+.v-main__wrap {
+  height: calc(100% - 130px) !important;
+}
+
+.v-btn .v-btn__content {
+  font-size: 15px !important;
+}
+
+.custom-btn {
+  font-size: 20px !important;
+}
+</style>
