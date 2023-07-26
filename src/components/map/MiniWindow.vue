875218f0df4cff866c7f964040ad3e442c032b1a
cjw0824 <128335544+cjw0824@users.noreply.github.com>
2023-07-26 12:14:22 +0900

    feat:[최지원] 수영장 상세정보 창에 url 이동 기능 추가 [SGV-MAP2]

--- src/components/map/MiniWindow.vue
+++ src/components/map/MiniWindow.vue
@@ -1,15 +1,38 @@
 <template lang="">
     <div class="black-bg">
-        <div class="white-bg">
-            <h2>미니창</h2>
-            {{ this.placeData }}
-            <button @click="$emit('close', false)">xxx</button>            
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Moirai+One&display=swap" rel="stylesheet">
+        <div class="white-bg form-floating mb-3">
+            <div style="text-align: center; padding-top:10px;">
+                <a :href=this.placeData.place_url class="titleLink">{{ this.placeData.title }}</a>
+            </div>
+            <!-- <h4 style="text-align: center; padding: 10px;">{{ this.placeData.title }}</h4>
+            <h4 style="text-align: center; padding: 10px;">{{ this.placeData.place_url }}</h4> -->
+            <br/>
+            <div class="row" style="text-align: center;">
+                <div class="col-sm-4"></div>
+                <div class="col-sm-4">
+                    <button type="button" class="btn btn-custom1"> 후기 남기기 </button>
+                </div>
+                <div class="col-sm-4"></div>
+            </div>
+            1<br/>
+            1<br/>
+            1<br/>
+            1<br/>
+            <div class="row" style="text-align: center;">
+                <div class="col-sm-10"></div>
+                <div class="col-sm-1">
+                    <button type="button" class="btn btn-custom1" @click="$emit('close', false)">xxx</button>            
+                </div>
+            </div>
         </div>
     </div>
 </template>
 <script>
 export default {
-    props : ['placeData']
+    props: ['placeData']
 }
 </script>
 <style>
@@ -19,11 +42,49 @@ export default {
     background: rgba(0, 0, 0, 0.399);
     position: fixed;
 }
+
 .white-bg {
     width: 50%;
-    height: 500px;
-    background: rgb(230, 244, 255);
+    height: 300px;
+    background: #EDFAFD;
     border-radius: 5px;
     margin: 15px auto;
+}
+
+.btn-custom1 {
+    --bs-btn-font-weight: 600;
+    --bs-btn-color: white;
+    --bs-btn-bg: #83c1e7;
+
+    /* --bs-btn-border-color: var(--bd-violet); */
+    --bs-btn-border-radius: .5rem;
+
+    /* 마우스 올렸을 때 폰트색 */
+    --bs-btn-hover-color: var(--bs-white);
+    --bs-btn-hover-bg: #2A93D5;
+    /* --bs-btn-hover-border-color: #{shade-color($bd-violet, 10%)}; */
+
+    /* --bs-btn-focus-shadow-rgb: var(--bd-violet-rgb); */
+    /* 클릭했을때 색 */
+    --bs-btn-active-color: white;
+    --bs-btn-active-bg: #2A93D5;
+    /* --bs-btn-active-border-color: #{shade-color($bd-violet, 20%)}; */
+}
+
+.titleLink {
+    text-decoration: none;
+    color: black;
+    font-size: xx-large;
+    font-weight: bold;
+    font-family: 'Moirai One', cursive;
+}
+
+.titleLink:hover {
+    color: #2A93D5;
+}
+
+.row {
+    margin-right: 0;
+    margin-left: 0;
 }
 </style>
