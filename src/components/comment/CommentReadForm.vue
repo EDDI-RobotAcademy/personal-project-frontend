ca3aef18d3e9fb627e95cbab267743d6e2137b85
YeoulCho <cyu5733@gmail.com>
2023-07-27 22:06:14 +0900

    update: [조여울] 댓글 수정 바로 반영[CGV-22]
    약간 어설픔

--- src/components/comment/CommentReadForm.vue
+++ src/components/comment/CommentReadForm.vue
@@ -29,105 +29,116 @@
 const commentModule = 'commentModule'
 import { mapActions } from 'vuex';
 export default {
-    props: { 
+    props: {
         board: {
             type: Object,
             required: true,
         },
     },
-    data(){
-        return{
-            text:'',
-            userToken:'',
+    data() {
+        return {
+            text: '',
+            userToken: '',
             isModify: false
         }
     },
-    methods:{
-        ...mapActions( 
-            commentModule, ['requestCommentModifyToSpring','requestDeleteCommentToSpring']
+    methods: {
+        ...mapActions(
+            commentModule, ['requestCommentModifyToSpring', 'requestDeleteCommentToSpring']
         ),
-        modifyComment(comment){
+        modifyComment(comment) {
             this.isModify = !this.isModify
-            if(!this.isModify){
-            const {text, commentId} = comment
-            const userToken = this.userToken
-            this.requestCommentModifyToSpring({text, userToken, commentId})
+            if (!this.isModify) {
+                const { text, commentId } = comment
+                const userToken = this.userToken
+                this.requestCommentModifyToSpring({ text, userToken, commentId })
+                    .then(() =>
+                        location.reload()
+                    )
             }
         },
-        deleteComment(commentId){
+        deleteComment(commentId) {
             this.requestDeleteCommentToSpring(commentId)
         }
     },
-    created(){
-        this.userToken=localStorage.getItem("userToken")
+    created() {
+        this.userToken = localStorage.getItem("userToken")
     },
 }
 </script>
 <style scoped>
-.comments_content{
+.comments_content {
     display: flex;
     flex-direction: row;
     text-align: right;
     justify-content: space-between;
 }
-.button_icon{
+
+.button_icon {
     margin-left: auto;
 }
-.comments_card{
+
+.comments_card {
     padding: 20px 0px;
-    width: 1000px; 
-    margin: 0px auto; 
+    width: 1000px;
+    margin: 0px auto;
     padding: 10px;
     box-shadow: 0px 3px 1px -2px rgba(0, 0, 0, 0.1), 0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 2px 0px rgba(0, 0, 0, 0.12)
 }
-.comments_ul{
+
+.comments_ul {
     padding-left: 15px;
     padding-right: 15px;
 }
 
-.comments_li{
+.comments_li {
     list-style-type: none;
     border-bottom: 1px solid #e6e6eb;
     padding: 20px 0px 10px 0px;
 }
-.comments_description{
+
+.comments_description {
     font-size: 15px;
     color: #505050;
     display: block;
     font-weight: bold;
     padding-left: 30px;
 }
-.comments_name{
+
+.comments_name {
     display: flex;
     flex-direction: row;
     margin-top: 5px;
     margin-bottom: 0px;
     /* justify-content: space-between; */
 }
-.logoImg{
-      max-height: 30px; 
-      max-width: 30px; 
-      aspect-ratio: 1;
-    }
-.comments_nickname{
-margin-right: 5px;
+
+.logoImg {
+    max-height: 30px;
+    max-width: 30px;
+    aspect-ratio: 1;
+}
+
+.comments_nickname {
+    margin-right: 5px;
 
 }
-.comments_postdate{
-color: grey;
-font-size: 15px;
-margin-left: auto;
+
+.comments_postdate {
+    color: grey;
+    font-size: 15px;
+    margin-left: auto;
 }
-.comments_description_modify:focus{
+
+.comments_description_modify:focus {
     outline: none;
 }
-.comments_description_modify{
+
+.comments_description_modify {
     width: 550px;
     font-size: 15px;
     color: #505050;
     display: block;
     font-weight: bold;
     padding-left: 30px;
-}
-
-</style>
+}</style>
