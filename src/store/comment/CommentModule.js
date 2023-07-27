9c42419e0a9621663300f479227d987b4208515c
YeoulCho <cyu5733@gmail.com>
2023-07-27 14:44:35 +0900

    chore: [조여울] 댓글 불필요 파일 정리 [CGV-20]

--- src/store/comment/CommentModule.js
+++ src/store/comment/CommentModule.js
@@ -1,17 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-import mutations from "@/store/comment/mutations"
 import actions from "@/store/comment/actions"
-import state from "@/store/comment/states"
 
 Vue.use(Vuex)
 
 const CommentModule = {
     namespaced: true,
-    state, 
-    actions, 
-    mutations,
+    actions,
 }
 
 export default CommentModule
