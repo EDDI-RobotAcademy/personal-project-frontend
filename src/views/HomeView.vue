93c52b3dc3dd0657e90b10c280454d4630047ef7
YeoulCho <cyu5733@gmail.com>
2023-07-30 04:01:18 +0900

    update: [조여울] 홈 화면 파일 정리 [CGV-8]

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,30 +5,16 @@
         <div>
           <p class="invite"> 당신을 위한 <br><span style="font-size: 1.5em">{{ your_location }}</span> 추천 카페</p>
         </div>
-        <v-btn @click="getLocation" class="mylocation" style="border-radius: 16px;"><v-icon> mdi-crosshairs-gps</v-icon> 내
-          위치중심</v-btn>
+        <v-btn @click="getLocation" class="mylocation" style="border-radius: 16px;">
+          <v-icon> mdi-crosshairs-gps</v-icon>
+          내 위치중심
+        </v-btn>
       </v-img>
     </v-row>
     <v-col>
-      <!-- <div class="BodyBox">
-        <section class="card_section">
-          <h1 class="card_section_title">#가성비</h1>
-          <VueperSlides class="no-shadow" :visible-slides="5" slide-multiple :gap="2" :slide-ratio="1 / 5"
-            :dragging-distance="200" :breakpoints="{ 800: { visibleSlides: 2, slideMultiple: 2 } }">
-            <VueperSlide v-for="(slide, i) in slides" :key="i" :image="slide.image">
-              <template #content>
-                <div class="slide_content_text">
-                  <h4>{{ slide.content }}</h4>
-                </div>
-              </template>
-            </VueperSlide>
-          </VueperSlides>
-        </section>
-      </div> -->
-
       <div class="BodyBox">
         <section class="card_section">
-          <h1 class="card_section_title">#아인슈페너</h1>
+          <h1 class="card_section_title">#인기 게시판</h1>
           <VueperSlides class="no-shadow" :visible-slides="5" slide-multiple :gap="2" :slide-ratio="1 / 5"
             :dragging-distance="200" :breakpoints="{ 800: { visibleSlides: 2, slideMultiple: 2 } }">
             <VueperSlide v-for="board in boards" :key="board.boardId" :image="getImageUrl(board)">
@@ -87,16 +73,14 @@ export default {
       if (!board || !board.filePathList || board.filePathList.length === 0) {
         return require('@/assets/icon/default.png');
       }
-      const filePath = board.filePathList[0];
+      const filePath = board.filePathList[0]
       if (!filePath || !filePath.imagePath) {
-        return require('@/assets/icon/default.png');
+        return require('@/assets/icon/default.png')
       }
-      return `https://${this.awsBucketName}.s3.${this.awsBucketRegion}.amazonaws.com/${filePath.imagePath}`;
-
-
+      return `https://${this.awsBucketName}.s3.${this.awsBucketRegion}.amazonaws.com/${filePath.imagePath}`
     },
     goToBoardDetails(boardId) {
-      this.$router.push(`/member-board-read-page/${boardId}`);
+      this.$router.push(`/member-board-read-page/${boardId}`)
     },
   },
   computed: {
