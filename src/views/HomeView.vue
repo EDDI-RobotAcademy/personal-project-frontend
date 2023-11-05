31b003d6597294ba9b30c87850ddc2241604a69f
cjw0824 <128335544+cjw0824@users.noreply.github.com>
2023-11-05 22:22:11 +0900

    feat:[최지원] home 페이지 추가 [SGV-HOME1]

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,73 @@
-<template>
-  <hello-world />
+<template lang="">
+  <div>
+    <NaviTest />
+    <div style="width:600px; padding-top: 190px; margin: auto">
+      <h1 style="text-align: center">환영합니다.</h1>
+      <h4 style="text-align: center">SwimminGo에서 당신의 수영 이야기를 들려주세요.</h4>
+      <!-- <button type="button" class="btn btn-outline-primary btn-lg" style="margin-top: 10px;">근처 수영장 확인하러 가기</button> -->
+      <!-- <img src="../assets/swgHome.png" style="width: 400px; display:block; margin: auto; margin-top: 70px;" @click="$router.push('/mapMarkerTestPage')"> -->
+      <div class="user-wrap">
+        <div class="user-image">
+          <img src="../assets/swgHome.png" @click="$router.push('/mapMarkerTestPage')">
+        </div>
+        <div class="user-text">
+          <b>근처 수영장 찾아보기</b>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
-
 <script>
-  import HelloWorld from '../components/HelloWorld'
-
-  export default {
-    name: 'Home',
-
-    components: {
-      HelloWorld,
-    },
-  }
+import NaviTest from '@/components/NaviTest.vue';
+export default {
+  components: {
+    NaviTest
+  },
+}
 </script>
+<style>
+/* .imgBox {
+  width: 400px;
+  display: block;
+  margin: auto;
+  margin-top: 70px;
+} */
+
+.user-wrap {
+  width: 500px;
+  margin: auto;
+  margin-top: 30px;
+  position: relative;
+}
+
+.user-wrap {
+  cursor: pointer;
+}
+
+.user-wrap img {
+  width: 500px;
+  vertical-align: middle;
+}
+
+.user-text {
+  position: absolute;
+  top: 45%;
+  left: 50%;
+  width: 100%;
+  margin: 10px auto;
+  transform: translate(-50%, -50%);
+  font-size: 30px;
+  font-family: 'ypseo';
+  color: white;
+  text-align: center;
+  display: none;
+}
+
+.user-image:hover {
+  filter: brightness(60%)
+}
+
+.user-image:hover~.user-text {
+  display: block;
+}
+</style>
